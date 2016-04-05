8926868c8f64dbcd97d63dfeea10ffc6b1bc7d50
orechova <vlassky.oriesok@yahoo.com>
2016-04-05 19:25:46 +0200

    Sort added (ascii, not unicode). Changes when sides are switched

--- www/js/controllers/VocabularyCtrl.js
+++ www/js/controllers/VocabularyCtrl.js
@@ -29,4 +29,32 @@
     }
   }]);
 
+
+app.directive('focusOn',function($timeout) {
+    return {
+        restrict : 'A',
+        link : function($scope,$element,$attr) {
+            $scope.$watch($attr.focusOn,function(_focusVal) {
+                $timeout(function() {
+                    //_focusVal ? $element.focus() : $element.blur();
+                    // TODO ?
+                });
+            });
+        }
+    }
+});
+
+app.filter('orderByUnicode',function(){
+  return function(array, parameter) {
+      if (array && array.length > 0){
+        array.sort(function(a, b){
+          return a[parameter].localeCompare(b[parameter]);
+        });
+        return array;
+      } else {
+        return [];
+      }
+    }
+});
+
 })();
